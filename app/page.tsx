1f96fb7d74582d68af1d8752a5715c246c6202d2
Hamza <hamza.abbad@gmail.com>
2024-09-12 22:25:33 +0100

    Added "Under construction" notification.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {AppShell, AppShellMain, Center, Group, Image, Text, Title, Avatar} from '@mantine/core';
+import {AppShell, AppShellMain, Notification, Text, Loader, Center, rem} from '@mantine/core';
 import Header from './components/header';
 
 export default function Page() {
@@ -6,6 +6,13 @@ export default function Page() {
     <AppShell padding="md" header={{height: 150}}>
       <Header name="Hamza Abbad" avatarURL="/avatar.svg" shortDescription="Engineer in Artificial Intelligence and Computer Science" />
       <AppShellMain>
+          <Center pos="fixed" bottom={rem(20)} left={rem(10)} right={rem(10)}>
+          <Notification icon={<Loader size={24} type="bars" color="yellow.6" />} withCloseButton={false} bg="yellow.0"
+                        title={<Text fw="bold" c="yellow.6">Under construction</Text>} color="transparent" shadow="xs"
+                        className="animate__animated animate__bounceInUp animate__delay-2s">
+              This website is under construction. I am working on it. Please check back in a few days.
+          </Notification>
+          </Center>
       </AppShellMain>
     </AppShell>
   );
