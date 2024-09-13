d6fcfb47fdd8a2f16ee9bd7d7cddb7322f6df295
Hamza <hamza.abbad@gmail.com>
2024-09-13 22:21:30 +0100

    Fix some errors in `notice.tsx`

--- app/components/notice.tsx
+++ app/components/notice.tsx
@@ -6,12 +6,12 @@ export function Notice() {
     return (
         <Center pos="fixed" bottom={rem(20)} left={rem(10)} right={rem(10)}>
             <Notification icon={<Loader size={24} type="bars" color="yellow.6" />} withCloseButton={false} bg="yellow.0"
-            title={<Text fw="bold" c="yellow.6">Work in progress</Text>} color="transparent" shadow="xs"
-            className="animate__animated animate__bounceInUp animate__delay-2s">
+            title={<Text fw="bold" c="yellow.6">Work in progress</Text>} color="transparent"
+            className="animate__animated animate__bounceInUp animate__delay-3s">
                 This website is under construction. In the meantime, you can check out my&nbsp;
                 <Anchor href="https://hamza5.github.io/InteractiveCV/" target="_blank" rel="noopener noreferrer"
                         underline="hover" size="sm" fw="bold" c="yellow.6"
-                        component={Link as never}>
+                        component={Link}>
                     Interactive CV
                 </Anchor>.
             </Notification>
