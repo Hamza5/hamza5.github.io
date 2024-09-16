2054f72f70679cb0a8c817855147937f6ccb7881
Hamza <hamza.abbad@gmail.com>
2024-09-16 22:35:52 +0100

    Added some animated glow.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,4 @@
-import {AppShellHeader, Text, Avatar, Stack, Title, Flex} from "@mantine/core";
+import {AppShellHeader, Text, Avatar, Stack, Title, Flex, Box} from "@mantine/core";
 import styles from "./header.module.css";
 import 'animate.css';
 
@@ -12,10 +12,12 @@ export default function Header(props: HeaderProps) {
     return (
         <AppShellHeader className={`${styles.banner} animate__animated animate__slideInDown`}>
             <Flex gap="md" h="100%" px="md" direction="row" align="center">
-                <Avatar src={props.avatarURL} alt={props.name} style={{zIndex: 10}}
-                        className={`${styles.circleShadow} animate__animated animate__rollIn animate__delay-1s`}
-                        h={{base: "70%", md: "80%"}} w="fit-content"
-                />
+                <Box className="animate__animated animate__rollIn animate__delay-1s"
+                     h={{base: "70%", md: "80%"}}>
+                    <Avatar src={props.avatarURL} alt={props.name} style={{zIndex: 10}}
+                            className={`${styles.circleShadow}`} w="fit-content" h="100%"
+                    />
+                </Box>
                 <Stack gap={0} align="start" w={{base: "70%", md: "auto"}}>
                     <Title order={1} className={`${styles.gradientText} animate__animated animate__fadeInLeft animate__delay-2s`}>{props.name}</Title>
                     <Text fw="bold" className={`${styles.gradientText} animate__animated animate__fadeInLeft animate__delay-3s`}>{props.shortDescription}</Text>
