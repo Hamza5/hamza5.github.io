921a6e8366affbe431b4af03f067870933cd2dd4
Hamza <hamza.abbad@gmail.com>
2024-09-29 14:35:33 +0100

    Used `img` tags to load external images.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import NextImage from 'next/image';
-import {AppShell, AppShellMain, Box, Group, Stack, Text, Button, Flex, Image} from '@mantine/core';
+import {AppShell, AppShellMain, Box, Group, Stack, Text, Button, Flex} from '@mantine/core';
 import {IconBrandGithub, IconBrandUpwork, IconExternalLink} from "@tabler/icons-react";
 import Header from './components/header';
 import {Notice} from "./components/notice";
@@ -38,12 +37,12 @@ export default function Page() {
                       Most used technologies
                   </Text>
                   <Flex justify="center" gap="xs">
-                      <Image src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white"
-                             alt="Python" title="Python" width={120} height={40} component={NextImage} radius="md"/>
-                      <Image src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"
-                             alt="JavaScript" title="JavaScript" width={150} height={40} component={NextImage} radius="md"/>
-                      <Image src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white"
-                             alt="Node.js" title="Node.js" width={120} height={40} component={NextImage} radius="md"/>
+                      <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white"
+                             alt="Python" title="Python" width={120} height={40}/>
+                      <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"
+                             alt="JavaScript" title="JavaScript" width={150} height={40}/>
+                      <img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white"
+                             alt="Node.js" title="Node.js" width={120} height={40}/>
                   </Flex>
               </Stack>
           </Stack>
